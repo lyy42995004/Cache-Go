ab9d7c2fb286178c4b6754d437eb3b075e3a619b
我不会用gitee <3519480004@qq.com>
2025-04-26 18:10:52 +0800

    client & registry

--- client.go
+++ client.go
@@ -1,10 +1,12 @@
 package cache
 
 import (
+	"context"
 	"fmt"
 	"time"
 
 	pb "github.com/lyy42995004/Cache-Go/pb"
+	"github.com/sirupsen/logrus"
 	clientv3 "go.etcd.io/etcd/client/v3"
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/credentials/insecure"
@@ -15,7 +17,7 @@ type Client struct {
 	svcName string           // 服务名称
 	etcdCli *clientv3.Client // etcd 客户端实例
 	conn    *grpc.ClientConn // gRPC 连接实例
-	grpcCli pb.GCacheClient  // gRPC 客户端实例
+	grpcCli pb.GCacheClient  // gcache服务的  gRPC 客户端实例
 }
 
 // 编译时，强制检查 Client 类型是否实现了 Peer 接口
@@ -36,6 +38,7 @@ func NewClient(addr, svcName string, etcdCli *clientv3.Client) (*Client, error) 
 	}
 
 	// 建立 gRPC 连接
+	// ToDo: Dial在v2版本会被弃用，改用NewClient
 	conn, err := grpc.Dial(addr,
 		grpc.WithTransportCredentials(insecure.NewCredentials()), // 使用不安全的传输凭证
 		grpc.WithBlock(),                                     // 阻塞直到连接成功或超时
@@ -43,24 +46,74 @@ func NewClient(addr, svcName string, etcdCli *clientv3.Client) (*Client, error) 
 		grpc.WithDefaultCallOptions(grpc.WaitForReady(true)), // 等待服务器准备好再发送请求
 	)
 	if err != nil {
-		return nil, fmt.Errorf("failed to dial server: %v", err)
+		return nil, fmt.Errorf("failed to create client: %v", err)
 	}
 
-	// 创建 GCache 客户端
+	// 创建 GCache 服务的客户端实例
 	grpcClient := pb.NewGCacheClient(conn)
 
-    client := &Client{
-        addr:    addr,
-        svcName: svcName,
-        etcdCli: etcdCli,
-        conn:    conn,
-        grpcCli: grpcClient,
-    }
+	client := &Client{
+		addr:    addr,
+		svcName: svcName,
+		etcdCli: etcdCli,
+		conn:    conn,
+		grpcCli: grpcClient,
+	}
 
-    return client, nil
+	return client, nil
 }
 
 // Get 实现 Peer 接口
-func Get(group, key string) ([]byte, error) {
+func (c *Client) Get(group, key string) ([]byte, error) {
+	ctx, cancel := context.WithTimeout(context.Background(), 3*time.Second)
+	defer cancel()
 
+	resp, err := c.grpcCli.Get(ctx, &pb.Request{
+		Group: group,
+		Key:   key,
+	})
+	if err != nil {
+		return nil, fmt.Errorf("failed to get value from gcache: %v", err)
+	}
+
+	return resp.GetValue(), nil
+}
+
+// Set 实现 Peer 接口
+func (c *Client) Set(ctx context.Context, group, key string, value []byte) error {
+	resp, err := c.grpcCli.Set(ctx, &pb.Request{
+		Group: group,
+		Key:   key,
+		Value: value,
+	})
+	if err != nil {
+		return fmt.Errorf("failed to set value to gcache: %v", err)
+	}
+	logrus.Infof("grpc set request resp: %+v", resp)
+
+	return nil
+}
+
+// Delete 实现 Peer 接口
+func (c *Client) Delete(group, key string) (bool, error) {
+	ctx, cancel := context.WithTimeout(context.Background(), 3*time.Second)
+	defer cancel()
+
+	resp, err := c.grpcCli.Delete(ctx, &pb.Request{
+		Group: group,
+		Key:   key,
+	})
+	if err != nil {
+		return false, fmt.Errorf("failed to delete value from gcache: %v", err)
+	}
+
+	return resp.GetValue(), nil
+}
+
+// Close 实现 Peer 接口
+func (c *Client) Close() error {
+	if c.conn != nil {
+		return c.conn.Close()
+	}
+	return nil
 }
--- peers.go
+++ peers.go
@@ -2,10 +2,15 @@ package cache
 
 import (
 	"context"
+	"fmt"
+	"log"
 	"sync"
 
 	"github.com/lyy42995004/Cache-Go/consistenthash"
+	clientv3 "go.etcd.io/etcd/client/v3"
 )
+
+const defaultSvcName = "g-cache"
 
 // PeerPicker 定义peer选择器的接口
 type PeerPicker interface {
@@ -23,10 +28,61 @@ type Peer interface {
 
 // ClientPicker 实现PeerPicker接口
 type ClientPicker struct {
-	selfAddr string
-	svcName string
-	mu sync.Mutex
-	consHash *consistenthash.Map
-	// clients map[string]*Client
+	mu       sync.RWMutex
+	selfAddr string              // 当前节点地址
+	svcName  string              // 服务名
+	consHash *consistenthash.Map // 一致性哈希算法的实现
+	clients  map[string]*Client  // 服务实例的地址与节点客户端的映射
+	etcdCli  *clientv3.Client    // etcd 服务
+	ctx      context.Context     // 控制与 etcd 服务的交互
+	cancel   context.CancelFunc  // 用于取消 ctx 上下文对象的函数
+}
 
+// PickerOption 定义配置选项
+type PickerOption func(*ClientPicker)
+
+// WithServiceName 设置服务名
+func WithServiceName(name string) PickerOption {
+	return func(cp *ClientPicker) {
+		cp.svcName = name
+	}
+}
+
+// PrintPeers 打印当前已发现的节点（用于调试）
+func (cp *ClientPicker) PrintPeers() {
+	cp.mu.RLock()
+	defer cp.mu.RUnlock()
+
+	log.Printf("当前已发现的节点:")
+	for addr := range cp.clients {
+		log.Printf("- %s", addr)
+	}
+}
+
+// NewClientPicker 创建新的 ClientPicker 实例 
+func NewClientPicker(addr string, opts ...PickerOption) (*ClientPicker, error) {
+	ctx, cancel := context.WithCancel(context.Background())
+	picker := &ClientPicker{
+		selfAddr: addr,
+		svcName: defaultSvcName,
+		clients: make(map[string]*Client),
+		consHash: consistenthash.New(),
+		ctx: ctx,
+		cancel: cancel,
+	}
+
+	for _, opt := range opts {
+		opt(picker)
+	}
+
+	cli, err := clientv3.New(clientv3.Config{
+		// Endpoints: registry,
+	})
+	if err != nil {
+		cancel()
+		return nil, fmt.Errorf("failed to create etcd client: %v", err)
+	}
+	picker.etcdCli = cli
+
+	return picker, nil
 }
